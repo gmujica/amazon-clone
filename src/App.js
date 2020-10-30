ac2e28ef3dd9244b961fd832d42f4df3c2826a35
gmujica <gregorymujica1993@gmail.com>
2020-10-30 16:29:43 -0500

    change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
-import Header from './compoents/Header'
-import Home from './compoents/Home'
-import Checkout from './compoents/Checkout'
+import Header from './Components/Header'
+import Home from './Components/Home'
+import Checkout from './Components/Checkout'
 
 function App() {
   return (
